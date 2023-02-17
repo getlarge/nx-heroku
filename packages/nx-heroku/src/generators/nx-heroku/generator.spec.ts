97e835532621b73ab857708996baed059945a7a7
getlarge <ed@getlarge.eu>
2023-02-17 13:47:58 +0100

    test: create suite for deploy target generator

--- packages/nx-heroku/src/generators/nx-heroku/generator.spec.ts
+++ packages/nx-heroku/src/generators/nx-heroku/generator.spec.ts
@@ -1,20 +1,43 @@
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+import {
+  Tree,
+  readProjectConfiguration,
+  addProjectConfiguration,
+} from '@nrwl/devkit';
 
 import generator from './generator';
 import { NxHerokuGeneratorSchema } from './schema';
 
 describe('nx-heroku generator', () => {
   let appTree: Tree;
-  const options: NxHerokuGeneratorSchema = { name: 'test' };
+  const options: NxHerokuGeneratorSchema = {
+    projectName: 'test',
+    org: 'test-org',
+  };
 
   beforeEach(() => {
     appTree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+    addProjectConfiguration(
+      appTree,
+      'test',
+      {
+        root: 'apps/test',
+        sourceRoot: 'apps/test/src',
+      },
+      false
+    );
   });
 
   it('should run successfully', async () => {
     await generator(appTree, options);
     const config = readProjectConfiguration(appTree, 'test');
     expect(config).toBeDefined();
+    expect(config.targets).toHaveProperty('deploy');
+    expect(config.targets.deploy).toEqual({
+      executor: '@aloes/nx-heroku:deploy',
+      options: {
+        org: 'test-org',
+      },
+    });
   });
 });
