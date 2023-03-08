0a174e219ffa7108b829dfc93c3d3ec9d2111937
Edouard <ed@getlarge.eu>
2023-03-08 07:49:12 +0100

    ci: refine release flow

---
+++ tools/create-release.js
@@ -0,0 +1,133 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { join, resolve } = require('path');
+const { extractFromChangelog } = require('./generate-changelog');
+
+// produce changelog to be piped to `gh release create <tag> -F-`
+// test with :
+// node tools/generate-changelog.js -s "1.0.0" | node -e "const fs = require('fs'); const stdinBuffer = fs.readFileSync(0); console.log(stdinBuffer.toString())"
+
+const changelogFileNames = ['CHANGELOG.md', 'Changelog.md', 'changelog.md'];
+
+const validateParameters = ({ projectName, filename, semver }) => {
+  if (!projectName && !filename && !semver) {
+    throw new Error('Missing required arguments');
+  }
+  if (projectName && (filename || semver)) {
+    throw new Error('Cannot specify both projectName and filename or version');
+  }
+  if ((filename && !semver) || (!filename && semver)) {
+    throw new Error('Should specify both filename and version');
+  }
+};
+
+const getProjectPath = (projectName) =>
+  join(process.cwd(), `packages/${projectName}`);
+
+// read root or project package.json
+const getPackageJson = (projectName) => {
+  const packageJsonPath = projectName
+    ? `${getProjectPath(projectName)}/package.json`
+    : join(process.cwd(), 'package.json');
+
+  try {
+    return require(packageJsonPath);
+  } catch (e) {
+    throw new Error(`No package.json found in ${packageJsonPath}`);
+  }
+};
+
+const getChangelogPath = (projectName) => {
+  let value;
+  for (const fileName of changelogFileNames) {
+    const absolutePath = projectName
+      ? join(getProjectPath(projectName), fileName)
+      : resolve(fileName);
+    if (fs.existsSync(absolutePath)) {
+      value = absolutePath;
+      break;
+    }
+  }
+  return value;
+};
+
+const getChangelog = (filename) => {
+  try {
+    return fs.readFileSync(filename, {
+      encoding: 'utf8',
+    });
+  } catch (e) {
+    throw new Error(`${filename} not found.`);
+  }
+};
+
+// Look for the tag in either X.Y.Z or vX.Y.X formats
+const getTagName = (semver, projectName) => {
+  const tags = execSync('git tag', { encoding: 'utf8' });
+  const searchString = projectName
+    ? `^${projectName}-(v?)${semver}$`
+    : `^(v?)${semver}$`;
+  const tagMatches = tags.match(new RegExp(searchString, 'gm'));
+  if (!tagMatches) {
+    throw new Error(`Tag ${semver} or v${semver} not found`);
+  }
+  return tagMatches[0];
+};
+
+(function () {
+  try {
+    const argv = yargs(hideBin(process.argv))
+      .usage('Usage: $0 [--filename CHANGELOG.md] [--semver 1.5.0]')
+      .usage('Usage: $0 [--project nx-heroku]')
+      .options({
+        projectName: {
+          description: 'Nx project name',
+          demandOption: false,
+          alias: 'p',
+          type: 'string',
+        },
+        filename: {
+          description: 'Full Changelog path',
+          demandOption: false,
+          alias: 'f',
+          type: 'string',
+        },
+        semver: {
+          description: 'Version to extract from changelog',
+          demandOption: false,
+          alias: 's',
+          type: 'string',
+        },
+      })
+      .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging',
+      }).argv;
+
+    validateParameters(argv);
+
+    const { projectName, filename, semver, verbose } = argv;
+    const changelogPath = projectName
+      ? getChangelogPath(projectName)
+      : path.resolve(process.cwd(), filename);
+
+    const changelog = getChangelog(changelogPath);
+    const version = projectName ? getPackageJson(projectName).version : semver;
+    const tag = getTagName(version, projectName);
+    const versionChangelog = extractFromChangelog({
+      changelog,
+      version,
+      verbose,
+    });
+
+    execSync(`gh release create ${tag} -n ${versionChangelog}`);
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+})();
--- tools/generate-changelog.js
+++ tools/generate-changelog.js
@@ -1,58 +1,9 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-
-// produce changelog to be piped to `gh release -F-`
+// produce changelog to be piped to `gh release create <tag> -F-`
 // test with :
-// node tools/utils/generate-changelog.js -s "1.0.0" | node -e "const fs = require('fs'); const stdinBuffer = fs.readFileSync(0); console.log(stdinBuffer.toString())"
-
-const changelogFileNames = ['CHANGELOG.md', 'Changelog.md', 'changelog.md'];
-
-// read root package.json
-const getPackageJson = () => {
-  try {
-    return require(process.cwd() + '/package.json');
-  } catch (e) {
-    throw new Error(`No package.json found in ${process.cwd()}`);
-  }
-};
-
-const getChangelogPath = () => {
-  let value;
-  for (const fileName of changelogFileNames) {
-    if (fs.existsSync(path.resolve(fileName))) {
-      value = fileName;
-      break;
-    }
-  }
-  return value;
-};
-
-// read root changelog
-const getChangelog = ({ filename }) => {
-  try {
-    return fs.readFileSync(path.resolve(process.cwd(), filename), {
-      encoding: 'utf8',
-    });
-  } catch (e) {
-    throw new Error(`No ${filename} found in ${process.cwd()}`);
-  }
-};
-
-// Look for the tag in either X.Y.Z or vX.Y.X formats
-const getTagName = (semver) => {
-  const tags = execSync('git tag', { encoding: 'utf8' });
-  const tagMatches = tags.match(new RegExp(`^(v?)${semver}$`, 'gm'));
-  if (!tagMatches) {
-    throw new Error(`Tag ${semver} or v${semver} not found`);
-  }
-  return tagMatches[0];
-};
+// node tools/generate-changelog.js -s "1.0.0" | node -e "const fs = require('fs'); const stdinBuffer = fs.readFileSync(0); console.log(stdinBuffer.toString())"
 
 // changelog
-function extractFromChangelog({ verbose, semver }, changelog) {
+function extractFromChangelog({ changelog, verbose, version }) {
   // accept various ways to specify version starting like
   // # 1.0
   // ## v1.0
@@ -60,7 +11,7 @@ function extractFromChangelog({ verbose, semver }, changelog) {
   const versionStartStringRe = '^##? \\[?v?';
   const versionStartRe = new RegExp(versionStartStringRe);
   const versionRe = new RegExp(
-    versionStartStringRe + semver.replace(/\./, '.')
+    versionStartStringRe + version.replace(/\./, '.')
   );
   const footerLinkRe = new RegExp('^\\[');
 
@@ -91,36 +42,6 @@ function extractFromChangelog({ verbose, semver }, changelog) {
     .trim();
 }
 
-const argv = yargs(hideBin(process.argv))
-  .usage('Usage: $0 [--filename CHANGELOG.md] [--semver 1.5.0]')
-  .options({
-    filename: {
-      description: 'Full Changelog path',
-      demandOption: false,
-      alias: 'f',
-      default: getChangelogPath(),
-    },
-    semver: {
-      description: 'Version to extract from changelog',
-      demandOption: false,
-      alias: 's',
-      default: getPackageJson().version,
-    },
-  })
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    description: 'Run with verbose logging',
-  }).argv;
-
-(function () {
-  try {
-    const changelog = getChangelog(argv);
-    const versionChangelog = extractFromChangelog(argv, changelog);
-    console.log(versionChangelog);
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-})();
+module.exports = {
+  extractFromChangelog,
+};
